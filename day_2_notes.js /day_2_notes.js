9a9b21d9913c326525df86e493afadd2ec0feafe
Miguel Maxberry <75222383+guelygrands@users.noreply.github.com>
2021-03-25 21:38:47 -0400

    This is my first commit

--- day_2_notes.js /day_2_notes.js
+++ day_2_notes.js /day_2_notes.js
@@ -1,1 +1,40 @@
-console.log(3 == "3")
+let myNum = 5;
+let myStr = '5'; //typeof tells you the data type
+
+
+
+//Git
+
+//Version control system
+// cd name of folder moves you down
+//ls list every thing imside
+// pwd--> specifies directory
+
+
+//to initiate a git reoository
+// git init
+//1) tracking stage
+
+//staging files for a commit
+//git add <file_name> git add .
+
+//git status tells you the status of your repository
+
+// To commit your changes, run the following command:
+
+//git commit -m "Your commit message here"
+
+
+//To view the commit:
+//git log
+// shows us commit hash, branch name, commit message, and other metadata 
+
+//Go to GitHub.com and click Create Repositiry
+//Name our repository, make sure you leave it public
+//click create repository
+//Copy the https url provided
+//go back to vscode
+//link your local repository with remote repository by typing;
+// git remote add origin <paste the link>
+//To push to remote repository
+//git push origin master
